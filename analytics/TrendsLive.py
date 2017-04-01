503925c4859f8660ff88cd2030b03fea485585d0
Rand0mUsername <njov996@gmail.com>
2017-04-02 00:35:59 +0200

    Finish analytics

--- analytics/TrendsLive.py
+++ analytics/TrendsLive.py
@@ -3,9 +3,10 @@ import string
 import psycopg2
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+from TrendMode import TrendMode
 
 class TrendsLive:
-	def __init__(self):
+    def __init__(self):
         connect_str = "dbname='deepweb' user='midza' host='10.120.194.45' password='midza555333!'" 
         self.conn = psycopg2.connect(connect_str) 
         self.cursor = self.conn.cursor() 
@@ -13,48 +14,54 @@ class TrendsLive:
     # date_d je datum od kog gledamo pre i posle
     # mode je enum:
     def get_trends(self, date_middle, trend_mode):
-    	# durationi oba
-    	if trend_mode == TrendMode.WEEK:
-    		delta = relativedelta(weeks = 1)
-    	elif trend_mode == TrendMode.MONTH:
-    		delta = relativedelta(months = 1)
-    	elif trend_mode == TrendMode.YEAR:
-    		delta = relativedelta(years = 1)
-    	else:
-    		raise("This should never happen")
-    	date_old = date_middle - delta
-    	date_new = min(datetime.now(), date_middle + delta)
-    	dur_old = (date_middle - date_old).days
-    	dur_new = (date_new - date_middle).days
-    	query = """SELECT COALESCE(tab1.word, tab2.word) AS word, COALESCE(t2, 0) / dur_new - COALESCE(t1, 0) / 365.0 as wpd_diff FROM
-(SELECT word, count(time) as t1 FROM occurences WHERE time > DATE '2015-01-01' AND time <  DATE '2016-01-01' GROUP BY word) tab1
-FULL OUTER JOIN
-(SELECT word, count(time) as t2 FROM occurences WHERE time > DATE '2017-01-01'  AND time < DATE '2018-01-01'  GROUP BY word) tab2
-ON tab1.word = tab2.word
-ORDER BY wpd_diff DESC
-LIMIT 10
-
-    	"""
-
+        # durationi oba
+        if trend_mode == TrendMode.WEEK:
+            delta = relativedelta(weeks = 1)
+        elif trend_mode == TrendMode.MONTH:
+            delta = relativedelta(months = 1)
+        elif trend_mode == TrendMode.YEAR:
+            delta = relativedelta(years = 1)
+        else:
+            raise("This should never happen")
+        date_old = date_middle - delta
+        date_new = min(datetime.now(), date_middle + delta)
+        dur_old = (date_middle - date_old).days
+        dur_new = (date_new - date_middle).days
+        query = """ SELECT COALESCE(t1.word, t2.word) AS word, 
+                           CAST(COALESCE(occ2, 0) AS REAL) / %s - CAST(COALESCE(occ1, 0) AS REAL) / %s AS wpd_diff,
+                           COALESCE(occ1, 0),
+                           COALESCE(occ2, 0)
+                    FROM (SELECT word, count(time) AS occ1 
+                          FROM occurences 
+                          WHERE time > %s AND time < %s 
+                          GROUP BY word) t1
+                    FULL OUTER JOIN
+                         (SELECT word, count(time) AS occ2
+                          FROM occurences
+                          WHERE time > %s AND time < %s
+                          GROUP BY word) t2
+                       ON t1.word = t2.word
+                       ORDER BY wpd_diff DESC
+                       LIMIT 10
+                """
         self.cursor.execute(query, (dur_new, dur_old, date_old, date_middle, date_middle, date_new))
         rows = self.cursor.fetchall()
+        return rows
 
-
+    def get_occurences(self, word):
+        self.cursor.execute("""SELECT time FROM occurences WHERE word = 'brate'""")
         rows = self.cursor.fetchall()
-        for row in rows:
-            text = row[0]
-            sentiment = self.sen.get_sentiment(text)
-            opinions.append(sentiment)
-        if len(opinions) == 0:
-            return "0 mentions of " + sample + "."
-        avg = sum(opinions) / float(len(opinions))
-        verdict = self.sen.get_verdict(avg)
-        return str(len(opinions)) + " mentions of " + sample + " being " + verdict + " overall."
+        dates = [row[0] for row in rows]
+        return dates
 
 def main():
+    t = TrendsLive()
+    mid = datetime.strptime('1.1.2017', '%d.%m.%Y')
+    rows = t.get_trends(mid, TrendMode.YEAR)
+    for row in rows:
+        dates = t.get_occurences(row[0])
+        print(row[0] + " (" + str(row[1]) + " - " + str(row[2]) + " " + str(row[3]) + "):")
+        print(dates)
 
-# plotovanje lokacija koje se pojavljuju + sentiment
-# neka rec + sentiment u vezi nje
-
-s = SentimentAnalyzer()
-print(s.get_sentiment('I love trains'))
+if __name__ == "__main__":
+    main()
